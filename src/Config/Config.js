3c594388ca42374319661ddb32cafc4cc2b35301
deep <gjb21885@gmail.com>
2024-02-06 17:54:12 +0530

    first commit

---
+++ src/Config/Config.js
@@ -0,0 +1,11 @@
+const Config = {
+
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PRJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    
+}
+
+export default Config
